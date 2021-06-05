8a80cc2d555ec10df7d3cbd911b1a0d3bbd8fcf8
9GAG <sonalanand102@gmail.com>
2021-06-05 18:56:19 +0530

    inserting list to the database

--- to-do-list-app/index.js
+++ to-do-list-app/index.js
@@ -3,50 +3,55 @@ const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser');
 const app = express();
+// app.use('/users', usersRouter);
+let router = express.Router();
 let name1 = [];
 
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({extended : true}));
- //creating databse connection
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }));
+//creating databse connection
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'todoList'
-  });
- //connecting 
-  connection.connect(function(err) {
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'todoList'
+});
+//connecting 
+connection.connect(function (err) {
     if (err) {
-      console.error('error connecting: ' + err.stack);
-      return;
+        console.error('error connecting: ' + err.stack);
+        return;
     }
-  
+
     console.log('mysql connected');
-  });
-
-//   creating database
-// app.get('/createdb', (req,res) => {
-//     let sql = 'CREATE DATABASE todoList';
-//     connection.query(sql, (err,result) => {
-//         if(err) throw err;
-//         console.log(result);
-//         res.send('Databse Created');
-//     });
-// });
-
-app.get('/',(req,res) => {
-    res.render("list",{newListItems: name1});
 });
 
-app.post('/',(req,res) => {
+
+
+app.get('/', (req, res) => {
+    res.render("list", { newListItems: name1 });
+});
+
+app.post('/', (req, res) => {
     let name = req.body.taskInput;
-    // console.log(name);
-    // res.render("list",{newListItem:name});
     name1.push(name);
-    res.redirect('/');
+
+     // insert list data into users table
+     var sql = `INSERT INTO list(listItem) VALUES (${name})`;
+     connection.query(sql, function (err, data) {
+         if (err) {
+             console.error('error connecting: ' + err.stack);
+             return;
+         }
+         console.log("User data is inserted successfully ");
+     });
+     res.redirect('/');  // redirect to user form page after inserting the data
+    
 });
 
-app.listen('3000',()=>{
+
+
+app.listen('3000', () => {
     console.log('listening to port 3000...');
 });
 
